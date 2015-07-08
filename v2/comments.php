1c64ec858eed0abf8c54b167e1e0dfc93e5b7e2e
cwoodford <chase@chasewoodford.com>
2015-07-07 20:08:19 -0400

    minor adjustment to logic

--- v2/comments.php
+++ v2/comments.php
@@ -12,8 +12,6 @@
 
     <?php elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-    <p>Comments are closed</p>
-
     <?php endif; ?>
 
     <?php comment_form(); ?>
