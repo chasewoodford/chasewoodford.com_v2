1be5d977e717fd542824cbdf122216c2c4a6534e
cwoodford <chase@chasewoodford.com>
2016-05-21 18:30:01 -0400

    fix social media links to match across site

--- work/digital/calmfocus.php
+++ work/digital/calmfocus.php
@@ -191,14 +191,8 @@
         <div class="twelve columns">
 
             <ul class="social-links">
-                <li><a href="https://www.facebook.com/chasewoodford" aria-label="Visit Chase Woodford's Facebook page" title="Visit Chase Woodford's Facebook page"><i class="fa fa-facebook"></i></a></li>
                 <li><a href="https://github.com/chasewoodford" aria-label="Visit Chase Woodford's GitHub page" title="Visit Chase Woodford's GitHub page"><i class="fa fa-github"></i></a></li>
-                <li><a href="https://plus.google.com/+ChaseWoodford/posts" aria-label="Visit Chase Woodford's Google Plus page" title="Visit Chase Woodford's Google Plus page"><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="http://instagram.com/chase1263070" aria-label="Visit Chase Woodford's Instagram page"  title="Visit Chase Woodford's Instagram page"><i class="fa fa-instagram"></i></a></li>
                 <li><a href="https://www.linkedin.com/in/chasewoodford" aria-label="Visit Chase Woodford's LinkedIn page" title="Visit Chase Woodford's LinkedIn page"><i class="fa fa-linkedin"></i></a></li>
-                <li><a href="http://www.pinterest.com/chasewoodford/" aria-label="Visit Chase Woodford's Pinterest page"  title="Visit Chase Woodford's Pinterest page"><i class="fa fa-pinterest"></i></a></li>
-                <li><a href="skype:woodmanjr2?call" aria-label="Skype with Chase Woodford" title="Skype with Chase Woodford"><i class="fa fa-skype"></i></a></li>
-                <li><a href="http://open.spotify.com/user/124506286" aria-label="Visit Chase Woodford's Spotify page" title="Visit Chase Woodford's Spotify page"><i class="fa fa-spotify"></i></a></li>
                 <li><a href="https://twitter.com/chase1263070" aria-label="Visit Chase Woodford's Twitter page" title="Visit Chase Woodford's Twitter page"><i class="fa fa-twitter"></i></a></li>
             </ul>
 
